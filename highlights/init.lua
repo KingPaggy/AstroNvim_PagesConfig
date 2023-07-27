5653966cca102a3c3e8fb581324b104fdc6d1abe
PageKim <kingpage12138@qq.com>
2023-07-27 16:09:49 +0800

    Force background to be transparent

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,5 @@
 return { -- this table overrides highlights in all themes
+  Normal = { guibg = nil, ctermbg = nil },
+  NonText = { guibg = nil, ctermbg = nil },
   -- Normal = { bg = "#000000" },
 }
